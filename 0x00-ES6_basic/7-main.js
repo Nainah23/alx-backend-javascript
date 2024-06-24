84e063f2578572f59507be7b301fa15e79e91eac
Nainah23 <133117208+Nainah23@users.noreply.github.com>
2024-06-24 14:24:07 +0300

    Handle getBudgetObject

---
+++ 0x00-ES6_basic/7-main.js
@@ -0,0 +1,5 @@
+/* eslint-disable linebreak-style */
+// eslint-disable-next-line import/extensions
+import getBudgetObject from './7-getBudgetObject.js';
+
+console.log(getBudgetObject(400, 700, 900));
